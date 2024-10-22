b39db1ef540c471e2dcc211cbc33d0020a1c0393
evan <evanbonner99@gmail.com>
2024-10-22 11:59:41 +0100

    Implement full review page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
           element={<FavoriteMoviesPage />}
         />
         <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/reviews/:id" element={<MovieReviewPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>

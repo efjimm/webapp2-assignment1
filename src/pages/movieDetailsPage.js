2c99b3339cbebb817f9c45bcb7abf41bb39ff64d
evan <evanbonner99@gmail.com>
2024-10-22 11:35:04 +0100

    Movie details from API

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import MovieHeader from "../components/headerMovie/";
 import MovieDetails from "../components/movieDetails/";
 import Grid from "@mui/material/Grid2";
@@ -6,8 +7,34 @@ import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 
 const MoviePage = (props) => {
-  const movie = props.movie;
-  const images = props.images;
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`,
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((movie) => {
+        // console.log(movie)
+        setMovie(movie);
+      });
+  }, [id]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`,
+    )
+      .then((res) => res.json())
+      .then((json) => json.posters)
+      .then((images) => {
+        // console.log(images)
+        setImages(images);
+      });
+  }, []);
 
   return (
     <>
@@ -15,12 +42,14 @@ const MoviePage = (props) => {
         <>
           <MovieHeader movie={movie} />
           <Grid container spacing={5} style={{ padding: "15px" }}>
-            <Grid size={{xs: 3}}>
-              <div sx={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "space-around",
-              }}>
+            <Grid size={{ xs: 3 }}>
+              <div
+                sx={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  justifyContent: "space-around",
+                }}
+              >
                 <ImageList
                   sx={{
                     height: "100vh",
@@ -28,20 +57,17 @@ const MoviePage = (props) => {
                   cols={1}
                 >
                   {images.map((image) => (
-                    <ImageListItem
-                      key={image}
-                      cols={1}
-                    >
+                    <ImageListItem key={image.file_path} cols={1}>
                       <img
-                        src={`https://image.tmdb.org/t/p/w500/${image}`}
-                        alt={image}
+                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                        alt={image.file_path}
                       />
                     </ImageListItem>
                   ))}
                 </ImageList>
               </div>
             </Grid>
-            <Grid size={{xs: 9}}>
+            <Grid size={{ xs: 9 }}>
               <MovieDetails movie={movie} />
             </Grid>
           </Grid>

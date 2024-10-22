ba468582c648b2ed074f00e81f087d59b6cbc462
evan <evanbonner99@gmail.com>
2024-10-22 13:32:46 +0100

    Clean up warnings

--- src/components/templateMoviePage/index.js
+++ src/components/templateMoviePage/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import MovieHeader from "../headerMovie";
 import Grid from "@mui/material/Grid2";
 import ImageList from "@mui/material/ImageList";
